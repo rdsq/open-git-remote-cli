c3270292afa3949ae293dad3c4c52a8563bb0107
rdsq <62446963+rdsq@users.noreply.github.com>
2024-11-10 23:39:06 +0200

    fix get remote kind of

--- src/get-remotes.ts
+++ src/get-remotes.ts
@@ -5,11 +5,19 @@ const regex = /^\S+ +\S/;
 export default async function getRemotes(repo: string): Promise<{
     [key: string]: string;
 } | null> {
-    const output: string = execSync('git remote -v', {
-        cwd: repo,
-    }).toString();
-    if (output.startsWith('fatal: not a git repository')) {
-        return null;
+    let output: string;
+    try {
+        output = execSync('git remote -v', {
+            cwd: repo,
+        }).toString();
+    } catch (err) {
+        if (err.message.startsWith('Command failed: git remote -v\nfatal: not a git repository')) {
+            console.error('This is not a git repository');
+            return null;
+        } else if (err.code === 'ENOENT') {
+            console.error('Not a directory');
+            return null;
+        } else throw err;
     }
     const lines = output.split('\n');
     lines.pop(); // remove the last empty line
