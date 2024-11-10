5975be5503c674e57f29fb1fad54ae6e59a64cff
rdsq <62446963+rdsq@users.noreply.github.com>
2024-11-10 22:49:29 +0200

    version

--- src/cli.ts
+++ src/cli.ts
@@ -2,11 +2,14 @@ export default function(argv: string[]): {
     path: string | null;
     remote: string | null;
     help: boolean;
+    version: boolean;
 } {
     const isHelp = argv.indexOf('--help') !== -1 || argv.indexOf('-h') !== -1;
+    const isVersion = argv.indexOf('--version') !== -1 || argv.indexOf('-v') !== -1;
     return {
         path: argv[0] ?? null,
         remote: argv[1] ?? null,
         help: isHelp,
+        version: isVersion,
     };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+#!/usr/bin/enc node
 import cliParser from './cli.js';
 import helpMessage from './help.js';
 import * as process from 'node:process';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
 
 const argv = process.argv.slice(2);
 
@@ -8,5 +11,13 @@ const parsed = cliParser(argv);
 
 if (parsed.help) {
     console.log(helpMessage);
+    process.exit(0);
+}
+
+if (parsed.version) {
+    const packageFilePath = path.join(import.meta.dirname, '../package.json');
+    const packageFileText = await fs.readFile(packageFilePath, 'utf-8');
+    const packageFile = JSON.parse(packageFileText);
+    console.log(packageFile.version);
     process.exit(0);
 }
