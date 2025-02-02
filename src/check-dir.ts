34fff6007a2253bf858307f1a13662b3be6bdf72
rdsq <62446963+rdsq@users.noreply.github.com>
2025-02-02 14:45:09 +0200

    change check-dir logic

--- src/check-dir.ts
+++ src/check-dir.ts
@@ -1,21 +1,17 @@
-import { readdir } from 'node:fs/promises';
+import { execSync } from 'node:child_process';
 import { exit } from 'node:process';
 
 export default async function checkDir(dir: string): Promise<void | never> {
     try {
-        const items = await readdir(dir);
-        if (items.indexOf('.git') === -1) {
-            console.error('Not a git repository');
-            exit(1);
-        }
+        execSync('git rev-parse --is-inside-work-tree', { cwd: dir });
     } catch (err) {
         const code: string | undefined = err.code;
         if (code === 'ENOENT') {
             console.error('No such directory');
-            exit(1);
         } else if (code === 'ENOTDIR') {
             console.error('Not a directory');
-            exit(1);
-        } else throw err;
+        }
+        // if it is a git error, it has already printed an error
+        exit(1);
     }
 }
